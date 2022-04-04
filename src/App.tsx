720ea97cf747c891eb754ba63237f49566786387
Raphael Voellmy <r.voellmy@gmail.com>
2022-04-04 16:54:44 +0200

    install @harmony-js/crypto and call toBech32()

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { toBech32 } from "@harmony-js/crypto";
+
 export default function App(): JSX.Element {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      {toBech32("0xAD8a7BDc0d195619A72aD41cE55DAC93A5bc896c")}
+    </div>
+  );
 }
